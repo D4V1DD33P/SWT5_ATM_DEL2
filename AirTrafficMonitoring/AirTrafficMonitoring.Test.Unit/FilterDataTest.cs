08b69ec1e20b88fd5501f238f0c5e9a663a4ffb4
D4V1DD33P <201404972@post.au.dk>
2018-11-22 20:36:01 +0100

    Setop af backup genskabt

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/FilterDataTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using ATMLibrary;
+using ATMLibrary.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
+
+
+
+namespace AirTrafficMonitoring.Test.Unit
+{
+    [TestFixture]
+   public class FilterDataTest
+    {
+        private IUpdateTrack _updateTrack;
+        private FilterData _uut;
+        private ITrackData _fakeTrackData;
+        private List<ITrackData> _fakeTrackDataList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeTrackDataList = new List<ITrackData>();
+            _updateTrack = Substitute.For<IUpdateTrack>();
+            _uut = new FilterData(_updateTrack);
+            _fakeTrackData = new TrackData
+            {
+                X = 20000,
+                Y = 30000,
+                Altitude = 10000
+            };
+            
+        }
+
+        [Test]
+        public void ConfirmTracks_TracksInArea_IsCorrect()
+        {
+            _fakeTrackDataList.Add(_fakeTrackData);
+
+            _uut.ConfirmTracks(_fakeTrackDataList);
+
+            _updateTrack.Received().Update(Arg.Is<List<ITrackData>>(x => x.Count == 1));
+        }
+    }
+}
---
+++ AirTrafficMonitoring/AirTrafficMonitoring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TransponderReceiver;
+using ATMLibrary.Interfaces;
+using ATMLibrary;
+
+
+namespace AirTrafficMonitoring
+{
+    class Program
+    {
+        static void Main(/*string[] args*/)
+        {
+            string str = "Vent... ";
+            ITransponderReceiver transponderDataReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+
+            ITrackData renderTrack = new TrackData();
+            IVicinityData vicinityData = new VicinityData();
+
+
+            IDetectVicinity vicinity = new DetectVicinity(vicinityData, vicinityData);
+            IUpdateTrack trackUpdate = new UpdateTrack(renderTrack, vicinity);
+            IFilterData filtering = new FilterData(trackUpdate);
+
+            var decoder = new ParseData(transponderDataReceiver, filtering);
+
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
