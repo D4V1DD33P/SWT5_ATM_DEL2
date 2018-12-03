d8bb4402c98a302b40d3f2feb7b3d1438a1a4b39
DannyTraan <201610981@post.au.dk>
2018-12-03 16:12:50 +0100

    Parsing is back on track

--- AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/ParsingTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/ParsingTest.cs
@@ -1,54 +1,55 @@
-﻿//using System.Collections.Generic;
-//using ATMLibrary;
-//using ATMLibrary.Interfaces;
-//using NSubstitute;
-//using NUnit.Framework;
-//using TransponderReceiver;
+﻿using System.Collections.Generic;
+using ATMLibrary;
+using ATMLibrary.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
 
 
-//namespace AirTrafficMonitoring.Test.Unit
-//{
+namespace AirTrafficMonitoring.Test.Unit
+{
 
-//    [TestFixture]
+    [TestFixture]
 
-//    class ParsingTest
-//    {
+    class ParsingTest
+    {
 
-//        private ParseData _uut;        // private Parsing _uut;
-//        private ITransponderReceiver _receiver;
-//        private RawTransponderDataEventArgs _fakeTransponderDataEventArgs;
-//        private FilterData _filtering;         //private IFiltering _filtering;
+        private ParseData _uut;  
+        private ITransponderReceiver _receiver;
+        private RawTransponderDataEventArgs _fakeTransponderDataEventArgs;
+        private IFilterData _filtering;       
 
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _receiver = Substitute.For<ITransponderReceiver>();
-//            _filtering = Substitute.For<FilterData>();
-//            _uut = new ParseData(_receiver, _filtering);
-//            _fakeTransponderDataEventArgs = new RawTransponderDataEventArgs(new List<string>()
-//                { "VXU610;12345;67890;12000;20162221100909221" });
+        [SetUp]
+        public void Setup()
+        {
+            _receiver = Substitute.For<ITransponderReceiver>();
+            _filtering = Substitute.For<IFilterData>();
+            _uut = new ParseData(_receiver, _filtering);
+            _fakeTransponderDataEventArgs = new RawTransponderDataEventArgs(new List<string>()
+                { "JAS001;12345;67890;12000;20160101100909111" });
 
-//        }
+        }
 
-//        private void RaiseFakeEvent()
-//        {
-//            _receiver.TransponderDataReady += Raise.EventWith(_fakeTransponderDataEventArgs);
-//        }
+        private void RaiseFakeEvent()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(_fakeTransponderDataEventArgs);
+        }
 
-//        [Test]
-//        public void OneTrackInList_CountCorrect()
-//        {
-//            RaiseFakeEvent();
-//            _filtering.Received().ConfirmTracks(Arg.Is<List<ITrackData>>(x => x.Count == 1));
-//        }
+        [Test]
+        public void OneTrackInList_CountCorrect()
+        {
+            RaiseFakeEvent();
+            _filtering.Received().ConfirmTracks(Arg.Is<List<ITrackData>>(x => x.Count == 1));
+        }
 
-//        [Test]
-//        public void OneTrackInList_TagCorrect()
-//        {
-//            RaiseFakeEvent();
-//            _filtering.Received().ConfirmTracks(Arg.Is<List<ITrackData>>(x => x[0].Tag == "VXU610"));
+        //Test af tag
+        [Test]
+        public void OneTrackInList_TagCorrect()
+        {
+            RaiseFakeEvent();
+            _filtering.Received().ConfirmTracks(Arg.Is<List<ITrackData>>(x => x[0].Tag == "JAS001"));
 
-//        }
-//    }
-//}
+        }
+    }
+}
